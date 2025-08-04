35874fe4b428fbef5639ca4843e1d9e7dc166dd0
sixtyfour <sixtyfouroftheclock@proton.me>
2025-08-04 06:19:55 -0300

    Testing CPU instructions

--- include/math/functions/square_root.hh
+++ include/math/functions/square_root.hh
@@ -2,7 +2,7 @@
 #define SQUARE_ROOT_GUARD
 namespace Untitled::Math
 {
-    double square_root(double number);
+    // double square_root(double number);
     float square_root(float number);
 }
 #endif // SQUARE_ROOT_GUARD
